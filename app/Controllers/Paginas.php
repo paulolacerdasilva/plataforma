4c5c0620fde9e43b7ff2ddb13f09c3b3493f70c4
paulolacerdasilva <plslacerda@gmail.com>
2025-06-30 16:35:13 -0400

    criacao do controller e pgs

--- app/Controllers/Paginas.php
+++ app/Controllers/Paginas.php
@@ -1,4 +1,13 @@
 <?php
-class Paginas{
-    
-}
+class Paginas extends Controller{
+    public function index(){
+        $dados = ['titulo'=>'Página Inicial', 'descricao'=>'Aula de PHP com MVC Orientada a Objetos.'
+        ];
+
+        $this->view('paginas/home', $dados);
+    }//fim da funcao index
+
+    public function sobre($id){
+        echo $id. '<hr>';
+    }//fim do método sobre
+}//fim da classe Paginas
--- app/Libraries/Rota.php
+++ app/Libraries/Rota.php
@@ -1,17 +1,26 @@
 <?php
     class Rota{
         private $controlador = 'Paginas';
+        private $metodo = 'index';
+        private $parametros = [];
+
         public function __construct(){
-           $url = $this->url();
+           $url = $this->url() ? $this->url():[0];
             if(file_exists('../app/Controllers/'.ucwords($url[0]).'.php')){
             $this->controlador = ucwords($url[0]);
             unset($url[0]);
             }
             require_once '../app/Controllers/'.$this->controlador.'.php';
             $this->controlador = new $this->controlador;
-
+            if(isset($url[1])){
+                if(method_exists($this->controlador, $url[1])){
+                    $this->metodo = $url[1];
+                    unset($url[1]);
+                }//fim do if que verifica se o método existe
+            }//fim do if que verica se a url existe 
+            $this->parametros = $url ? array_values($url): [];
+            call_user_func_array([$this->controlador, $this->metodo], $this->parametros);
             var_dump($this);
-
         }//fim do construtor
 
         private function url(){
